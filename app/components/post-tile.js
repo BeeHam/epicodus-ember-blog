56dad0740212580b0358b01bb870375560d3a6aa
Janice Dunn & Brigham Thompson <student@epicodus.com>
2015-09-23 13:48:51 -0700

    Delete a single blog post.

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  action: {
-    
+  actions: {
+    delete(blogpost) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        this.sendAction('destroyBlogpost', blogpost);
+      }
+    }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('blogpost');
+  },
+
+  actions: {
+    destroyBlogpost(blogpost) {
+      blogpost.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
